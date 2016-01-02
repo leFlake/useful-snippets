d8eeedad88cea29f53514c3ab1b84ae27a38c520
leFlake <social@nielscordes.de>
2016-01-02 13:58:18 +0100

    Add knuth shuffle

---
+++ javascript/knuth-shuffle.js
@@ -0,0 +1,18 @@
+/**
+ * Shuffle an array using the Knuth shuffle (or Fisher-Yates shuffle, depending
+ * on what you like :p)
+ *
+ * https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ *
+ * @param array
+ * @returns {*}
+ */
+function shuffleArray(array) {
+    for(var i = 0; i < array.length-1; i++) {
+        var j = Math.floor(Math.random() * array.length-i);
+        var array_temp = array[i];
+        array[i] = array[i+j];
+        array[i+j] = array_temp;
+    }
+    return array;
+}
